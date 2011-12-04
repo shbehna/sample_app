6a07c839e8cae4cfe146fc7a9d9f44e4ddfbe49e
Sherif Behna <shbehna@gmail.com>
2011-12-03 19:01:38 -0500

    Chapter 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,26 @@
 class UsersController < ApplicationController
   
   def new
+    @user = User.new
     @title = "Sign up"
   end
   
   def show
     @user = User.find(params[:id])
     @title = "Show user - " + @user.name
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      @user.password = nil
+      @user.password_confirmation = nil
+      render 'new'
+    end
   end
 
 end
